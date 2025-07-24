81f410cd354e75f875e657fe0f2b71dddaeae9c9
arnugrha <arnugrha1@gmail.com>
2025-07-24 19:23:26 +0700

    menambahkan paragraf ke 2

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quisquam aliquam totam unde sed consequuntur? Libero possimus voluptatibus reiciendis, nesciunt, odio placeat provident repellendus obcaecati nobis ab incidunt numquam dolorem.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quo dolores aperiam ipsum unde laboriosam!</p>
 </body>
 </html>
